9cfcd53ad38b21d1b82ded60d01bef1a8316e37b
Rens Rutten <Rensrutten@Live.nl>
2020-12-15 14:03:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/madlevel6example/TriviaApiService.kt
@@ -0,0 +1,13 @@
+package com.example.madlevel6example
+
+import retrofit2.http.GET
+
+
+interface TriviaApiService {
+
+    // The GET method needed to retrieve a random number trivia.
+    @GET("/random/trivia?json")
+    suspend fun getRandomNumberTrivia(): Trivia
+}
+
+
